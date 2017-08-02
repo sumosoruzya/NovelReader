26d0596906bc7a6572c57a93e7b7c73eb443ecc8
sumosoruzya <sumosoruzya@gmail.com>
2017-08-02 19:54:26 +0900

    web view test

--- NovelReader/SecondViewController.swift
+++ NovelReader/SecondViewController.swift
@@ -8,11 +8,44 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UIWebViewDelegate {
 
+    let webView : UIWebView = UIWebView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Delegate設定
+        webView.delegate = self
+        
+        // Webページの大きさを画面に合わせる
+        let rect:CGRect = self.view.frame
+        webView.frame = rect
+        webView.scalesPageToFit = true
+        
+        // インスタンスをビューに追加する
+        self.view.addSubview(webView)
+        
+        // URLを指定
+        let url: URL = URL(string: "https://www.apple.com/iphone/")!
+        let request: URLRequest = URLRequest(url: url)
+        
+        // リクエストを投げる
+        webView.loadRequest(request)
+        
+    }
+    
+    
+    // ロード時にインジケータをまわす
+    func webViewDidStartLoad(_ webView: UIWebView) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+        print("indicator on")
+    }
+    
+    // ロード完了でインジケータ非表示
+    func webViewDidFinishLoad(_ webView: UIWebView) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = false
+        print("indicator off")
     }
 
     override func didReceiveMemoryWarning() {
